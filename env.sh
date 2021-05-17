57c7b06f5ba113650b8f43deb96b9468862569a4
Nathan Cleyet-Marrel <nathancm@free.fr>
2021-05-17 15:09:54 +0200

    docker-compose

--- env.sh
+++ env.sh
@@ -1,3 +1,19 @@
+# InfluxDB env vars
+export INFLUXDB_USERNAME=admin
+export INFLUXDB_PASSWORD=auraadmin
+export INFLUXDB_DATABASE=hackathon
+
+# Grafana env vars
+export GRAFANA_USERNAME=admin
+export GRAFANA_PASSWORD=admin
+
+# PostgreSQL env vars
+export POSTGRES_HOST_URL=$(hostname -I | cut -d ' ' -f 1)
+export POSTGRES_DATABASE=postgres
+export POSTGRES_USER=postgres
+export POSTGRES_PASSWORD=postgres
+
+# Airflow env vars
 export AIRFLOW_HOME=$(pwd)
 export AIRFLOW_CONFIG=$AIRFLOW_HOME/airflow.cfg
 export PYTHONPATH=$(pwd)
